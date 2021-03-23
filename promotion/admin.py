bc3f5b68e7d2e54ff9473ef393e8df6f1dd784e0
Vladimir <vl.shev@list.ru>
2021-03-23 21:24:47 +0300

    Add promotion app, add models to this

---
+++ promotion/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Review)
+admin.site.register(Performer)
+admin.site.register(Customer)
+admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(Deal)
+admin.site.register(Message)
